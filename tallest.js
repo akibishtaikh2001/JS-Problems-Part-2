c085de169833a88faad56d9680c67ff3f44926b4
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-11 10:53:32 +0600

    Max and Min value using loop and function

---
+++ tallest.js
@@ -0,0 +1,31 @@
+const height = [65, 66, 68, 72, 78, 60, 65, 66];
+
+function getMax(numbers) {
+    let max = numbers[0];
+    for (const number of numbers) {
+        if (number > max) {
+            max = number;
+        }
+
+    }
+    return max;
+
+}
+
+const max = getMax(height);
+console.log('max value is', max);
+
+function getMin(numbers) {
+    let min = numbers[0];
+    for (const number of numbers) {
+        if (number < min) {
+            min = number;
+        }
+
+    }
+    return min;
+
+}
+
+const min = getMin(height);
+console.log('max value is', min);
