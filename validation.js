ab4a15bfa700a62dc8715f9ceb42cdaa700c0a2f
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-14 00:56:19 +0600

    Handle Unexpected Function Input Parameter Error

---
+++ validation.js
@@ -0,0 +1,50 @@
+function mulitiply(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'please provide a number'
+    }
+    const mult = num1 * num2;
+    return mult;
+}
+const result = mulitiply(5, 'seven');
+// console.log(result);
+
+
+function fullName(first, second) {
+    if (typeof first !== 'string') {
+        return 'First name should be a string';
+    }
+    else if (typeof second !== 'string') {
+        return 'Second Name should be a string';
+    }
+    const full = first + ' ' + second;
+    return full;
+}
+
+const full = fullName('Akimal')
+// console.log(full);
+
+function getPrice(product) {
+    if (typeof product !== 'object') {
+        return 'Please provide an object'
+    }
+
+
+    const price = product.price;
+    return price;
+}
+
+// const price = getPrice({ name: 'chulkani dandi', price: 35, color: 'blue' })
+const price = getPrice(5)
+// console.log(price);
+
+function getSecond(numbers) {
+    if (Array.isArray(numbers) === false) {
+        return 'pleace provide an Array'
+    }
+
+    const second = numbers[1];
+    return second;
+}
+
+const second = getSecond([45, 67, 21]);
+console.log(second);
