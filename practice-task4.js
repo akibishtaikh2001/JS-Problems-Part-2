a81767be1842155f936a50d466c962f8bc108bbf
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-16 12:31:10 +0600

    Complete Practice Task 4

---
+++ practice-task4.js
@@ -0,0 +1,22 @@
+   const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+
+    function findAveragePhonePrice(phones){
+        let price = 0;
+        for(const phone of phones){
+            price = price + phone.price;
+            
+        }
+        return price / phones.length;
+    }
+
+
+    const phoneAverage = findAveragePhonePrice(phones);
+    console.log(phoneAverage);
+    
